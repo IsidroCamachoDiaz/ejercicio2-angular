16b3c902287d656ffb7fb10675a4296c82340aca
ISIDRO\isidr <isidroswitch@gmail.com>
2024-01-22 11:25:24 +0100

    22-1-2024

--- src/app/vistas/bienvenida/bienvenida.component.ts
+++ src/app/vistas/bienvenida/bienvenida.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from 'src/app/shared/services/user';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { BaseDeDatosService } from 'src/app/servicios/base-de-datos.service';
+
 
 @Component({
   selector: 'app-bienvenida',
@@ -10,8 +13,17 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 })
 export class BienvenidaComponent  implements OnInit{
   Roles: any = ['Admin', 'Author', 'Reader'];
-  constructor(private router: Router,private f:FormBuilder,public authService: AuthService) {}
+  constructor(private router: Router,private f:FormBuilder,public authService: AuthService,private fbs:BaseDeDatosService) {}
   ngOnInit(): void {
+  }
+  usuario:User={
+    id: 9,
+    email: "string",
+    nombre: "string",
+    foto: "",
+    emailVerified: false,
+    rol: '',
+    contrasenia: ''
   }
 
   formulario=this.f.group({
@@ -25,4 +37,27 @@ export class BienvenidaComponent  implements OnInit{
   irAlEnlace() {
     this.mostrarBienvenida = false;  // Cambia la visibilidad para mostrar el nuevo componente
   }
+
+  anadirFormulario(){
+  this.usuario.email=this.formulario.get("email").value;
+  const constrasenia=this.formulario.get("contrasenia").value;
+
+  this.fbs.queyCollection("usuarios","correo",this.usuario.email).subscribe(res=>{
+    this.usuario=res[0];
+    if(this.usuario.id){
+      alert("El correo y/o contraseña son incorrectos")
+    }
+    else{
+      if(this.usuario.contrasenia!=constrasenia){
+        alert("El correo y/o contraseña son incorrectos")
+      }
+      else{
+        alert("Accedio Correctamente")
+        this.irAlEnlace();
+      }
+    }
+  })
+  }
 }
+
+
